fecbadd2e6a62d83ffebae6b80246c7abac05e65
Emre Faik Gökçe <emrefaikgokce@gmail.com>
2020-12-11 15:45:07 +0100

    Coding Challege 2 is completed

--- src/main.c
+++ src/main.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
-
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  main
- *  Description:  
- * =====================================================================================
- */
+#include <string.h>
+#include <espl_lib.h>
 
 int main ()
 {
+    int number_input;
+	char *number_input_in_words;	
+    bool continue_program = true;
+    char *user_answer;
 
-	printf("Hello ESPL\n");	
-	printf("A difference\n");
+    while(continue_program) {
+	    
+        printf("Please enter a number: ");
+	    scanf("%d", &number_input);
+	
+	    number_input_in_words = num_to_words(number_input);
+
+	    printf("%s\n", number_input_in_words);		
+
+    LOOP:{
+	        printf("Do you wish to continue? (y/n): ");
+            scanf(" %s", user_answer);
+    
+            if( !strcmp(user_answer, "y") ) {
+                continue_program = true;
+            }
+
+            else if ( !strcmp (user_answer, "n") ) {
+                continue_program = false;
+            }
+        
+            else {
+                printf ("Please enter y or n.\n");
+                goto LOOP;
+            }
+         }
+     }
 
 	return 0;
 }			
